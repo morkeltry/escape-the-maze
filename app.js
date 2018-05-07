aacbf72166551683fe6611289ef3b34c0eb59661
malcolm twhoo <morkeltry@mailfence.com>
2018-05-07 17:04:39 +0100

    shows player in magenta; follow() moves player; rotate({}) rotates arrow; None of this visible - no update pattern yet

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const gridWidth = 10;
 const gridHeight = 10;
-const pixelWidth = 80;
-const pixelHeight = 80;
+const pixelWidth = 75;
+const pixelHeight = 75;
 const pixelRadius = 35;
 const fillColour = d => 'pink';
 const strokeColour =  d => 'lightgrey';
+const arrowUpPath = 'M-15,-5 L0,-25 L15,-5 M0,-25 L0,30';
 
 const lookup = {};
+const current = '4,1';
 
 const populateSquares = (startArr,x,yInit,pattern) => {
   let count=0;
@@ -18,7 +20,8 @@ const populateSquares = (startArr,x,yInit,pattern) => {
         id : index,
         xPos : (x+0.5)*pixelWidth,
         yPos : (y+0.5)*pixelHeight,
-        direction : pattern(index)
+        direction : pattern(index),
+        hasPlayer : (index===current)
       });
       lookup[index] = count;
       count++;
@@ -31,12 +34,34 @@ const random = index => {
 }
 
 const contents = populateSquares([],gridWidth,gridHeight,random);
-    console.log(lookup);
 
+const doRotate = square => {
+  contents[lookup[square.id]].direction = (contents[lookup[square.id]].direction+90)%360;
+}
 
+const follow = index => {
+  doRotate ({ id : index });
+  let [x,y] = index.split(',');
+  [x,y] = [x*1,y*1];
+  switch (contents[lookup[index]].direction) {
+    case 0 : {y--; break;}
+    case 90 : {x++; break;}
+    case 180 : {y++; break;}
+    case 270 : {x--; break;}
+    default: break;
+  };
+  let newIndex = x +','+ y;
+  if (lookup[newIndex]>=0) {
+    contents[lookup[index]].hasPlayer = false;
+    contents[lookup[newIndex]].hasPlayer = true;
+  }
+  return newIndex;
+}
 
 
 const canBeReached = d => {
+  if (d.hasPlayer)
+    return ('magenta');
   let [x,y] = d.id.split(',');
   [x,y] = [x*1,y*1];
   surroundingIndices = [[-1,0,90],[0,-1,180],[1,0,270],[0,1,0]].map (xY =>
@@ -44,14 +69,17 @@ const canBeReached = d => {
       index : (x+xY[0]) +','+ (y+xY[1]),
       direction : xY[2]
     }));
-  return surroundingIndices.some (el =>{
-    return lookup[el.index]>=0 && el.direction === contents[lookup[el.index]].direction
-  })
+  return surroundingIndices.some (el =>
+    lookup[el.index]>=0 && el.direction === contents[lookup[el.index]].direction
+  )
   ? ('green')
     : ('lightgreen');
-}
+};
 
-const arrowUpPath = 'M-15,-5 L0,-25 L15,-5 M0,-25 L0,30'
+
+const doClick = d => {
+  console.log ('HI');
+}
 
 
 const maze = d3.select ('#maze');
@@ -61,7 +89,6 @@ maze
     .attr ('width',(gridWidth+1)*pixelWidth)
     .attr ('height',(gridHeight+1)*pixelHeight)
 
-
 let signs= squares.
   data (contents)
       .enter ()
@@ -70,16 +97,15 @@ let signs= squares.
 signs
   .classed ('square',true)
   .attr ('id', d=> d.id)
-  .attr ('cx', d => d.xPos)
-  .attr ('cy',d => d. yPos)
   .attr ('fill', canBeReached)
   .attr ('stroke', strokeColour)
   .attr ('stroke-width', 1)
+  .on ('click', doRotate)
 
 signs
     .append ('circle')
-  .attr ('cx', d => d.xPos)
-  .attr ('cy',d => d. yPos)
+      .attr ('cx', d => d.xPos)
+      .attr ('cy',d => d. yPos)
       .attr ('r', pixelRadius)
       .attr ('stroke-width', 1)
 signs
